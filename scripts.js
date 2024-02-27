59e88f563efd375377289c8eb200266991581663
Renan Gabriel Bueno <137081787+renangbueno@users.noreply.github.com>
2024-02-27 14:59:57 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ const date = new Date();
 const today = date.getDate();
 
 function getUser(){
-    var username = "";
     var localuser = document.getElementById("username").value;
     if (localuser != ""){
         sessionStorage.setItem("user", localuser);
@@ -12,6 +11,18 @@ function getUser(){
         alert("PREENCHA OS CAMPOS!");
     }
 }
+
+function fazerReserva(event) {
+  var button = event.target;
+  var idLista = button.id.replace('btnReservar-', ''); // Extrai a parte do ID após o traço
+  var ul = document.getElementById('alunos-' + idLista);
+
+  var li = document.createElement('li');
+  li.textContent = 'Novo Aluno';
+  ul.appendChild(li);
+}
+
+document.getElementById("btnReservar").addEventListener("click", fazerReserva);
 
 var dayContainer = document.getElementById("days");
 for (let i = today-1;i<=today+1;i++){
@@ -26,4 +37,23 @@ for (let i = today-1;i<=today+1;i++){
     }
 
     dayContainer.appendChild(singleDay);
+}
+
+var acc = document.getElementsByClassName("subtexto");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var painel = this.nextElementSibling;
+    if (painel.style.display === "block") {
+      painel.style.display = "none";
+    } else {
+      painel.style.display = "block";
+    }
+  });
 }
